4ac9e09ea007dab2b2d9a25f80e8a4fc8c398cd2
zhangyuanming <1195476689@qq.com>
2022-04-17 23:20:33 +0800

    项目初始化结构

---
+++ gulimall-prodoct/src/main/java/com/atguigu/gulimall/prodoct/GulimallProdoctApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.prodoct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallProdoctApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallProdoctApplication.class, args);
+    }
+
+}
---
+++ gulimall-prodoct/src/test/java/com/atguigu/gulimall/prodoct/GulimallProdoctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.prodoct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProdoctApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
