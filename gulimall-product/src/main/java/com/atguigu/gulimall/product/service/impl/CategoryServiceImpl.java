70c09aedda34561c87ad6c691eb70dde6fcb7a7a
zhangyuanming <1195476689@qq.com>
2022-04-25 11:33:38 +0800

    三级列表

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,66 @@
+package com.atguigu.gulimall.product.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.atguigu.common.utils.PageUtils;
+import com.atguigu.common.utils.Query;
+
+import com.atguigu.gulimall.product.dao.CategoryDao;
+import com.atguigu.gulimall.product.entity.CategoryEntity;
+import com.atguigu.gulimall.product.service.CategoryService;
+
+
+@Service("categoryService")
+public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity> implements CategoryService {
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        List<CategoryEntity> categoryEntities = baseMapper.selectList(null);
+        List<CategoryEntity> listLevel1Menus = categoryEntities.stream().filter(
+                categoryEntity -> categoryEntity.getParentCid().longValue() == 0
+        ).map(menu -> {
+            menu.setChildren(getChildrens(menu, categoryEntities));
+            return menu;
+        }).sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort()))
+                ).collect(Collectors.toList());
+        return listLevel1Menus;
+    }
+
+    /**
+     * 递归查找子菜单
+     *
+     * @param father
+     * @param all
+     * @return
+     */
+    private List<CategoryEntity> getChildrens(CategoryEntity father, List<CategoryEntity> all) {
+        List<CategoryEntity> childrens = all.stream().filter(categoryEntity -> {
+            return categoryEntity.getParentCid().longValue() == father.getCatId().longValue();
+        }).map(categoryEntity -> {
+            categoryEntity.setChildren(getChildrens(categoryEntity, all));
+            return categoryEntity;
+        }).sorted((menu1, menu2) -> {
+            return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort());
+        }).collect(Collectors.toList());
+        return childrens;
+    }
+
+    @Override
+    public PageUtils queryPage(Map<String, Object> params) {
+        IPage<CategoryEntity> page = this.page(
+                new Query<CategoryEntity>().getPage(params),
+                new QueryWrapper<CategoryEntity>()
+        );
+
+        return new PageUtils(page);
+    }
+
+}
