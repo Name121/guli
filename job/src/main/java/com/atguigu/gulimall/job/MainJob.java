3bb655a8d09b31e7f3a8234ce8c2c8884d2a269b
zhangyuanming <1195476689@qq.com>
2022-04-27 14:07:52 +0800

    bug

---
+++ job/src/main/java/com/atguigu/gulimall/job/MainJob.java
@@ -0,0 +1,16 @@
+package com.atguigu.gulimall.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * @author ZhangYuanMing
+ * @create 2022-04-27 11:33
+ */
+@SpringBootApplication
+public class MainJob {
+    public static void main(String[] args) {
+        SpringApplication.run(MainJob.class,args);
+    }
+}
